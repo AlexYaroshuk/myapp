439a75e9c38f1c46cfc71ce39a3529f58036c74c
majorluck <alexander_4288@Yahoo.com>
2022-12-25 19:40:19 +0800

    step 3

--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -22,6 +22,7 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
             onPressed: () async{
               final user = FirebaseAuth.instance.currentUser;
               await user?.sendEmailVerification();
+              await FirebaseAuth.instance.currentUser?.reload();
             },
             child: const Text('Send email verification'),)
         ],
