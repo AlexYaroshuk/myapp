42cdbcf4a7dbd9ca749459e60bdfed30de85bca1
majorluck <alexander_4288@Yahoo.com>
2022-12-25 15:17:34 +0800

    step 2

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,16 +1,18 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class LoginView extends StatefulWidget {
-  const LoginView({Key? key}) : super(key: key);
+
+class RegisterView extends StatefulWidget {
+  const RegisterView({Key? key}) : super(key: key);
 
   @override
-  State<LoginView> createState() => _LoginViewState();
+  State<RegisterView> createState() => _RegisterViewState();
 }
 
-class _LoginViewState extends State<LoginView> {
+class _RegisterViewState extends State<RegisterView> {
   late final TextEditingController _email;
   late final TextEditingController _password;
+
 @override
   void initState() {
     _email = TextEditingController();
@@ -27,8 +29,7 @@ void dispose() {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login')
+      appBar: AppBar(title: const Text('Register'),
       ),
       body: Column(
               children: [
@@ -55,31 +56,34 @@ void dispose() {
                       final email = _email.text;
                       final password = _password.text;
                       try {
-                        final userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+                      final userCredential =
+                          await FirebaseAuth.instance.createUserWithEmailAndPassword(
                         email: email,
                         password: password
                         );
                         print(userCredential);
-                      } on FirebaseAuthException catch (e) {
-                        if (e.code == 'user-not-found') {
-                          print('user not found');
-                        }
-                        else if (e.code == 'wrong-password')  {
-                          print('wrong password');
-                        }
+                    } on FirebaseAuthException catch (e) {
+                      if (e.code == 'weak-password') {
+                      print('Weak password');
+                      } else if (e.code == 'email-already-in-use') {
+                        print('Email is already in use');
+                      } else if (e.code == 'invalid-email') {
+                        print('Invalid email entered');
+                      }
     
                       }
+    
                     },
-                    child: const Text('Login'),
+                    child: const Text('Register'),
                 ),
                 TextButton(
                   onPressed: () {
-                    Navigator.of(context).pushNamedAndRemoveUntil(
-                      '/register/',
-                      (route) => false,
-                    );
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                  '/login/',
+                  (route) => false,);
                   },
-                  child: const Text('Create account'))
-              ]
+                  child: const Text('Already registered? Login here'),
+                  )
+              ],
             ),
     );}}
