a9e1fa99f5b6e7f7a3078e5b5287cfcbeb8f7e80
majorluck <alexander_4288@Yahoo.com>
2022-12-25 17:36:03 +0800

    STEP2

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 import 'package:myapp/views/login_view.dart';
 import 'package:myapp/views/register_view.dart';
-import 'firebase_options.dart';
+import 'package:myapp/views/verify_email_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,29 +24,50 @@ void main() {
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
-    @override
+  @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: Firebase.initializeApp(
-                    options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-             /*  final user = FirebaseAuth.instance.currentUser;
-                if (user?.emailVerified ?? false) {
-                  return const Text ('Done');
-                } else {
-                  return const VerifyEmailView();
-                } */
-                return const LoginView();
-               default:
-                return const CircularProgressIndicator();
-          }
-          }
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      ),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print('Email is verified');
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text('Done');
+          default:
+            return const CircularProgressIndicator();
+        }
+      },
+    );
+  }
+}
 
-      );
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main UI')
+      ),
+    );
   }
 }
